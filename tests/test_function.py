7e9f6196b637fd18602fa4b654325638935a639f
Rustem Galiullin <rustem.galiullin@bayanat.ai>
2023-06-06 07:00:44 +0400

    convert file tests to fixtures

--- tests/test_function.py
+++ tests/test_function.py
@@ -10,17 +10,29 @@ TEST_DATA_DIR = Path(__file__).parent / "data" / "requests"
 
 @pytest.fixture(scope="module")
 def message_verify():
-    return _load_json(TEST_DATA_DIR / "verify_event.json")
+    return {
+        "queryStringParameters": {
+            "hub.challenge": "73736871",
+            "hub.mode": "subscribe",
+            "hub.verify_token": "HAPPY_CODING",
+        }
+    }
 
 
 @pytest.fixture(scope="module")
 def message_read():
-    return _load_json(TEST_DATA_DIR / "status_read.json")
+    return {
+        "queryStringParameters": {},
+        "body": '{"object":"whatsapp_business_account","entry":[{"id":"105650032446631","changes":[{"value":{"messaging_product":"whatsapp","metadata":{"display_phone_number":"15550668492","phone_number_id":"108587615483446"},"statuses":[{"id":"wamid.HBgMOTcxNTU2NzY2MTc3FQIAERgSMDlCREFFRjI1QkUzQjY4RDU0AA==","status":"read","timestamp":"1675909679","recipient_id":"971556667777"}]},"field":"messages"}]}]}',
+    }
 
 
 @pytest.fixture(scope="module")
 def message_received():
-    return _load_json(TEST_DATA_DIR / "message.json")
+    return {
+        "queryStringParameters": {},
+        "body": '{"object":"whatsapp_business_account","entry":[{"id":"1111122222221","changes":[{"value":{"messaging_product":"whatsapp","metadata":{"display_phone_number":"1555555555","phone_number_id":"108108108108108"},"contacts":[{"profile":{"name":"Test User"},"wa_id":"971556667777"}],"messages":[{"from":"971556667777","id":"wamid.HBgMOTcxNTU2NzY2MTc3FQIAEhgUM0VCMDVENEM1QTUzNEI0RDI0NjYA","timestamp":"1675825342","text":{"body":"hello world"},"type":"text"}]},"field":"messages"}]}]}',
+    }
 
 
 def test_verify(message_verify, message_read, message_received):
