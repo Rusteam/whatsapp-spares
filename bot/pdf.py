7a77f42edd641b5618648877c6c8aa7bc4de992d
Rustem Galiullin <rustem.galiullin@bayanat.ai>
2023-07-23 19:30:19 +0400

    parse pdf order from european

---
+++ bot/pdf.py
@@ -0,0 +1,145 @@
+import re
+from abc import ABC, abstractmethod
+from dataclasses import dataclass
+from typing import Any, Optional
+
+import pandas as pd
+import pdfplumber
+import pydantic
+
+from bot.scheme import Currency, PartOrder
+
+TABLE_TYPE = list[PartOrder]
+
+
+@dataclass
+class PdfOrderProcessor(ABC):
+    """
+    Base class to extract data from pdf files and format into our data format.
+    """
+
+    file: str
+    checksum: Optional[float] = None
+
+    @property
+    @abstractmethod
+    def supplier_name(self) -> str:
+        pass
+
+    @property
+    @abstractmethod
+    def currency(self) -> Currency:
+        pass
+
+    @property
+    @abstractmethod
+    def vat(self) -> float:
+        pass
+
+    def _read_pdf(self):
+        pdf = pdfplumber.open(self.file)
+        return pdf
+
+    def _process_page_table(self, page) -> pd.DataFrame | None:
+        table = page.extract_table(
+            table_settings={
+                "vertical_strategy": "lines",
+                "horizontal_strategy": "text",
+                "min_words_horizontal": 2,
+            }
+        )
+        if not table:
+            return None
+        rows = self.parse_table(table)
+        df = pd.DataFrame([row.dict() for row in rows])
+        return df
+
+    def run(self):
+        pdf = self._read_pdf()
+        results = []
+        for page in pdf.pages:
+            if (t := self._process_page_table(page)) is not None:
+                results.append(t)
+
+        results = pd.concat(results)
+        if self.checksum:
+            if (res := self.calculate_total(results)) - self.checksum > 1e-4:
+                raise ValueError(f"Parsing has failed: {self.checksum=} and {res=}")
+        return results
+
+    @staticmethod
+    def concat_rows(prev_row: list, new_row: list) -> None:
+        for i in range(len(new_row)):
+            if new_row[i]:
+                prev_row[i] += " " + new_row[i]
+
+    @abstractmethod
+    def parse_table(self, table) -> TABLE_TYPE:
+        """From pdfplumbers extract_table output to a list of rows and
+        a list of column names."""
+        pass
+
+    def calculate_total(self, results: pd.DataFrame) -> float:
+        """Multiple quantity by price and add vat."""
+        total = results["quantity"] * results["price"]
+        return total.sum() * (1 + self.vat)
+
+
+class PdfOrderEuropeanAutospares(PdfOrderProcessor):
+    @property
+    def supplier_name(self):
+        return "European Autospares"
+
+    @property
+    def vat(self):
+        return 0.05
+
+    @property
+    def currency(self):
+        return Currency.aed
+
+    @staticmethod
+    def fix_number(value: str) -> str:
+        value = re.sub(",", "", value)
+        value = re.sub("\.00$", "", value)
+        return value
+
+    def _process_row(self, row: list) -> list:
+        # Strip manufacturer letter and remove non-word chars
+        part_num = row[1]
+        if m := re.match("(\w\s)(.+(\s\w+)?)", part_num):
+            row[1] = re.sub("\W", "", m.group(2))
+        # create a representation
+        try:
+            part_order = PartOrder(
+                part_number=row[1],
+                part_name=row[2],
+                price=self.fix_number(row[5]),
+                quantity=self.fix_number(row[4]),
+                currency=self.currency,
+                discount=row[6],
+            )
+        except pydantic.ValidationError as e:
+            print(f"{row=}")
+            raise e
+        return part_order
+
+    def parse_table(self, rows: list[Any]) -> TABLE_TYPE:
+        """Only keep meaningful rows and extract column names"""
+        rows = rows[1:]
+        rows_filtered = list(filter(lambda x: any(x), rows))
+        i = 1
+        while i < len(rows_filtered):
+            row = rows_filtered[i]
+            if row[0] == "":
+                # second line printing
+                prev_row = rows_filtered[i - 1]
+                self.concat_rows(prev_row, rows_filtered.pop(i))
+            elif row[0] is None:
+                # table subtotals
+                rows_filtered.pop(i)
+            else:
+                i += 1
+        # strip a manufacturer name
+        rows_filtered = list(map(lambda x: self._process_row(x), rows_filtered))
+        return rows_filtered
--- bot/scheme.py
+++ bot/scheme.py
@@ -1,11 +1,18 @@
 import os
+from enum import Enum
 from typing import Optional
 
 import pydantic
 
+
+class Currency(str, Enum):
+    aed = "AED"
+    rub = "RUB"
+    usd = "USD"
+    eur = "EUR"
+
+
 # pylint: disable=no-member
-
-
 class InputMessage(pydantic.BaseModel):
     """A raw message from a supplier."""
 
@@ -13,7 +20,7 @@ class InputMessage(pydantic.BaseModel):
     part_number: Optional[str] = None
     lead_days: int = 1
     vat: bool = True
-    currency: str = "AED"
+    currency: str = Currency.aed
 
 
 class OutputMessage(pydantic.BaseModel):
@@ -22,7 +29,7 @@ class OutputMessage(pydantic.BaseModel):
     price: float
     lead_days: int
     part_number: Optional[str] = None
-    currency: str = "RUB"
+    currency: str = Currency.rub
     weight: float = 0.0
 
     def format(self) -> str:
@@ -48,16 +55,13 @@ class Constants(pydantic.BaseModel):
     shipping_rate: float = float(os.getenv("SHIPPING_RATE_AED", "40"))
 
 
-class PartQuote(pydantic.BaseModel):
+class PartBase(pydantic.BaseModel):
     part_number: str = pydantic.Field(
         ...,
         description="Long part number including spaces and other non-alphanumeric characters.",
     )
     part_name: str = pydantic.Field(None, description="Short part description.")
     price: float = pydantic.Field(0.0, description="Price in the original currency.")
-    lead_time_days: int = pydantic.Field(
-        -1, description="Lead time in days, -1 for back order"
-    )
 
     # pylint: disable=no-self-argument
     @pydantic.validator("part_number")
@@ -67,3 +71,18 @@ class PartQuote(pydantic.BaseModel):
     @pydantic.validator("part_name")
     def capitalize(cls, v):
         return v.lower().capitalize()
+
+
+class PartQuote(PartBase):
+    lead_time_days: int = pydantic.Field(
+        -1, description="Lead time in days, -1 for back order"
+    )
+
+
+class PartOrder(PartBase):
+    quantity: int = pydantic.Field(None, description="How many items ordered.")
+    currency: str = pydantic.Field(Currency.aed, description="3-letter cuurency code.")
+    discount: float = pydantic.Field(0.0, description="discount on the items.")
+    manufacturer: str = pydantic.Field(
+        None, description="The original producer of the part."
+    )
